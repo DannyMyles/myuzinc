bbadfe125cc50136f85c0e7e091005d34c5656ce
DannyMyles <dannymyles1999@gmail.com>
2022-05-07 00:47:53 +0300

    deployment

--- src/assets/js/style.js
+++ src/assets/js/style.js
@@ -1,3 +1,4 @@
+// side bar
 jQuery(function ($) {
 
     $(".sidebar-dropdown > a").click(function() {
@@ -29,3 +30,74 @@ jQuery(function ($) {
   $(".page-wrapper").addClass("toggled");
   }); 
   });
+// end sidebar
+
+// impact story 
+$('.team-slider').owlCarousel({
+  loop: true,            
+  nav: false,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  smartSpeed: 450,
+  margin: 20,
+  responsive: {
+      0: {
+          items: 1
+      },
+      768: {
+          items: 2
+      },
+      991: {
+          items: 3
+      },
+      1200: {
+          items: 3
+      },
+      1920: {
+          items: 3
+      }
+  }
+});
+
+// end impact story 
+
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "70%";
+    // document.getElementById("flipkart-navbar").style.width = "50%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = "rgba(0,0,0,0)";
+}
+
+
+var btnDelete = document.getElementById('clear');
+      var inputFocus = document.getElementById('inputFocus');
+      //- btnDelete.on('click', function(e) {
+      //-   e.preventDefault();
+      //-   inputFocus.classList.add('isFocus')
+      //- })
+      //- inputFocus.addEventListener('click', function() {
+      //-   this.classList.add('isFocus')
+      //- })
+      btnDelete.addEventListener('click', function(e)
+      {
+        e.preventDefault();
+        inputFocus.value = ''
+      })
+      document.addEventListener('click', function(e)
+      {
+        if (document.getElementById('first').contains(e.target))
+        {
+          inputFocus.classList.add('isFocus')
+        }
+        else
+        {
+          // Clicked outside the box
+          inputFocus.classList.remove('isFocus')
+        }
+      });
+
+
